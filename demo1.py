462e88350afe477a43d3f22425e25a3778e1bc7c
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-10-04 16:01:45 +0530

    Add files via upload

---
+++ demo1.py
@@ -0,0 +1,31 @@
+#print("HelloWorld")
+'''print("Hello\nWorld")
+if 5 > 2:
+print("Five greater than two")
+if 5 > 2:
+    print("Five greater than two")
+if 5 > 2:
+         print("Five greater than two")
+if 5 > 2:
+    print("Five greater than two")
+     print("Five greater than two")
+x=6
+x="web"
+print(x)
+x=2j
+y=int(3)
+z=float(3)
+print(type(x))
+print(type(z))
+print(type(x))
+
+
+
+x,y,z="python ","is ","awesome"
+print(x+y+z)
+
+username=input("Enter username?")
+print("username is",username)
+'''
+a="Hello World"
+print(a[2:])
