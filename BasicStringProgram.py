392676de04cc4f7ebb4c3a6a5a0b31bec4db2635
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-10-19 14:38:13 +0530

    Add files via upload

---
+++ BasicStringProgram.py
@@ -0,0 +1,13 @@
+"""
+Author: Dawn K Vinod
+Date: 19-10-2024
+Description: Create, concatenate, and print a string and access a sub-string from a given string.
+"""
+
+first_name="John"
+last_name="Doe"
+
+full_name=first_name + " " + last_name
+print(full_name)
+
+print(full_name[5:])
