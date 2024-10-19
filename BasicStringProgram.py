f86e5ff1ed4f91f830f97da30cdc0d0b5e83fcfe
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-10-19 15:11:14 +0530

    BasicStringProgram.py

--- BasicStringProgram.py
+++ BasicStringProgram.py
@@ -4,10 +4,17 @@ Date: 19-10-2024
 Description: Create, concatenate, and print a string and access a sub-string from a given string.
 """
 
-first_name="John"
-last_name="Doe"
+first_name=input("Enter your first name:")
+last_name=input("Enter your last name:")
 
 full_name=first_name + " " + last_name
 print(full_name)
 
-print(full_name[5:])
+#length of first name
+len_firstname=len(first_name)
+
+#Extracting last name from full name
+print(full_name[len_firstname+1:])
+
+#Extracting first name from full name
+print(full_name[:len_firstname])
