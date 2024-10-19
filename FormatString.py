dfef522ef0f7f6bc5d47fa0bfa8524dbbe3362ec
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-10-19 14:25:44 +0530

    Add files via upload

---
+++ FormatString.py
@@ -0,0 +1,8 @@
+"""
+Author: Dawn K Vinod
+Date: 19-10-2024
+Description:Python program to print the following string in a specific format.
+Sample String : "Twinkle, twinkle, little star, How I wonder what you are! Up above the world so high,
+Like a diamond in the sky. Twinkle, twinkle, little star, How I wonder what you are"
+"""
+print("Twinkle, twinkle, little star,\n\t\tHow I wonder what you are! \n\t\t\t\tUp above the world so high, \n\t\t\t\tLike a diamond in the sky. \nTwinkle, twinkle, little star, \n\t\tHow I wonder what you are")
