84b4e5fd58888fc1efbef523125cae4a9d5a4a18
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-10-25 14:29:22 +0530

    Add files via upload

---
+++ find_Largest_Of_Two_Numbers.py
@@ -0,0 +1,12 @@
+"""
+Author: Dawn K Vinod
+Date: 25-10-2024
+Description: Write a Python program to find the largest of two numbers.
+"""
+num1=int(input("Enter the first number:"))
+num2=int(input("Enter the second number:"))
+
+if num1>num2:
+    print(num1,"is greater than",num2)
+else:
+    print(num2,"is greater than",num1)
