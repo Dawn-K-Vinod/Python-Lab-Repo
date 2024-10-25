7d0db16bbb5c453432e890f53b00278972ff5b19
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-10-25 14:53:49 +0530

    Update find_Largest_Of_Three_Numbers.py

--- find_Largest_Of_Three_Numbers.py
+++ find_Largest_Of_Three_Numbers.py
@@ -6,7 +6,6 @@ Description: Python program to find the largest of three numbers.
 num1=int(input("Enter first number:"))
 num2=int(input("Enter second number:"))
 num3=int(input("Enter third number:"))
-largest=0
 
 if num1>num2 and num1>num3:
     print(num1,"is largest.")
