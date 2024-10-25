67745e19d165035cd02693e92f90b0490924bbba
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-10-25 14:36:36 +0530

    Add files via upload

---
+++ positive_or_negative.py
@@ -0,0 +1,14 @@
+"""
+Author: Dawn K Vinod
+Date: 25-10-2024
+Description: Python program to check whether the given number is positive or negative.
+"""
+number=int(input("Enter a number:"))
+if number>0:
+    print(number,"is positive")
+elif number<0:
+    print(number, "is negative")
+else:
+    print("The given number is zero")
+
+
