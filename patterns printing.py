6388e202c4e961e882190bae2da9529853a45221
Dawn-K-Vinod <dawnkvinod6@gmail.com>
2024-11-22 16:03:26 +0530

    Update patterns printing.py

--- patterns printing.py
+++ patterns printing.py
@@ -3,31 +3,32 @@ Author: Dawn K Vinod
 Date: 22-11-2024
 Description: Print different star patterns using nested loop.
 """
+rows=10
 print("Increasing triangle:")
-for row in range(1,6):
+for row in range(1,rows+1):
     for j in range(row):
         print("*",end=" ")
     print(" ")
 
 
 print("\nDecreasing Triangle:")
-for row in range(5,0,-1):
+for row in range(rows,0,-1):
     for j in range(row):
         print("*",end=" ")
     print(" ")
 
-
+rows=8
 print("\nHill Pattern:")
-for row in range(1,6):
-    for space in range(5-row):
+for row in range(1,rows+1):
+    for space in range(rows-row):
         print(" ",end=" ")
     for star in range((row*2)-1):
         print("*",end=" ")
     print()
 
 print("\nReverse Hill Pattern:")
-for row in range(5,0,-1):
-    for space in range(5-row):
+for row in range(rows,0,-1):
+    for space in range(rows-row):
         print(" ",end=" ")
     for star in range((row*2)-1):
         print("*",end=" ")
